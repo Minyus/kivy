5efd86d3b646fc58247d5afd07cd41c54aa612e7
Minyus <yusuke.minami86@gmail.com>
2019-08-22 11:21:55 +0800

    Set MIN_REQUIRED_GL_VERSION = (1, 0)

--- kivy/core/gl/__init__.py
+++ kivy/core/gl/__init__.py
@@ -10,7 +10,7 @@ core provider can select an OpenGL ES or a 'classic' desktop OpenGL library.
 import sys
 from os import environ
 
-MIN_REQUIRED_GL_VERSION = (2, 0)
+MIN_REQUIRED_GL_VERSION = (1, 0)  # (2, 0)
 
 
 def msgbox(message):
